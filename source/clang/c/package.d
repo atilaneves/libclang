8f98a8797bbdf0ff9ae2b3ab5e26d816b8f07f0a
Atila Neves <atila.neves@gmail.com>
2018-03-15 17:53:17 +0100

    Better clang.c

--- source/clang/c/package.d
+++ source/clang/c/package.d
@@ -2,3 +2,8 @@
    D translations of the libclang C headers.
  */
 module clang.c;
+
+public import clang.c.index;
+public import clang.c.CXString;
+public import clang.c.CXErrorCode;
+public import clang.c.CXCompilationDatabase;
