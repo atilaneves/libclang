368d6162d6e5dc3a561f42229394be9666ffd0be
Atila Neves <atila.neves@gmail.com>
2018-03-15 22:34:39 +0100

    No utl build

--- reggaefile.d
+++ reggaefile.d
@@ -5,9 +5,4 @@ enum commonUtFlags = "-g -debug -cov";
 alias ut = dubTestTarget!(CompilerFlags(commonUtFlags),
                           LinkerFlags(),
                           No.allTogether);
-alias utl = dubConfigurationTarget!(Configuration("utl"),
-                                    CompilerFlags(commonUtFlags ~ " -unittest"),
-                                    LinkerFlags(),
-                                    Yes.main,
-                                    No.allTogether);
-mixin build!(ut, utl);
+mixin build!(ut);
