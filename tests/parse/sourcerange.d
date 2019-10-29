5ecb9244f2d37baa105f24becd3d8dfe976d30d7
Atila Neves <atila.neves@gmail.com>
2019-10-29 13:42:43 +0100

    Lazy SourceLocation member variables in SourceRange
    SourceRange now constructs its `start` and `end` variables on demand, abusing
    undefined behaviour by casting away `const`.

---
+++ tests/parse/sourcerange.d
@@ -0,0 +1,58 @@
+module parse.sourcerange;
+
+
+import test.infra;
+import clang;
+
+
+@("path")
+@safe unittest {
+    import clang.c.index: CXType_Pointer;
+    with(NewTranslationUnit("foo.cpp",
+                            q{
+                                const char* newString();
+                            }))
+    {
+        const cursor = translUnit.cursor;
+        const function_ = cursor.children[0];
+        function_.sourceRange.path.should == inSandboxPath("foo.cpp");
+    }
+}
+
+
+@("start")
+@safe unittest {
+    import clang.c.index: CXType_Pointer;
+    with(NewTranslationUnit("foo.cpp",
+                            q{
+                                const char* newString();
+                            }))
+    {
+        const cursor = translUnit.cursor;
+        const function_ = cursor.children[0];
+
+        function_.sourceRange.start.path.should == inSandboxPath("foo.cpp");
+        function_.sourceRange.start.line.should == 2;
+        function_.sourceRange.start.column.should == 33;
+        function_.sourceRange.start.offset.should == 33;
+    }
+}
+
+
+@("end")
+@safe unittest {
+    import clang.c.index: CXType_Pointer;
+    with(NewTranslationUnit("foo.cpp",
+                            q{
+                                const char* newString();
+                            }))
+    {
+        const cursor = translUnit.cursor;
+        const function_ = cursor.children[0];
+
+        function_.sourceRange.end.path.should == inSandboxPath("foo.cpp");
+        function_.sourceRange.end.line.should == 2;
+        function_.sourceRange.end.column.should == 56;
+        function_.sourceRange.end.offset.should == 56;
+    }
+}
--- tests/ut_main.d
+++ tests/ut_main.d
@@ -6,6 +6,7 @@ int main(string[] args) {
     return args.runTests!(
         "parse.raw",
         "parse.cooked",
+        "parse.sourcerange",
         "wrap",
         "ut.enum_",
     );
