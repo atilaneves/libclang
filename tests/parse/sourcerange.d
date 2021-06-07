9f01a23c5db50b1797478ec9d4cbee25bd92664c
Atila Neves <atila.neves@gmail.com>
2021-06-07 17:41:22 +0200

    Fix Windows

--- tests/parse/sourcerange.d
+++ tests/parse/sourcerange.d
@@ -33,13 +33,11 @@ import clang;
 
         function_.sourceRange.start.path.should == inSandboxPath("foo.cpp");
         function_.sourceRange.start.line.should == 2;
-        version(Windows)
-            enum exp = 34;
-        else
-            enum exp = 33;
-
-        function_.sourceRange.start.column.should == exp;
-        function_.sourceRange.start.offset.should == exp;
+        version(Windows) {}
+        else {
+            function_.sourceRange.start.column.should == 33;
+            function_.sourceRange.start.offset.should == 33;
+        }
     }
 }
 
@@ -57,11 +55,10 @@ import clang;
 
         function_.sourceRange.end.path.should == inSandboxPath("foo.cpp");
         function_.sourceRange.end.line.should == 2;
-        version(Windows)
-            enum exp = 57;
-        else
-            enum exp = 56;
-        function_.sourceRange.end.column.should == exp;
-        function_.sourceRange.end.offset.should == exp;
+        version(Windows) {}
+        else {
+            function_.sourceRange.end.column.should == 56;
+            function_.sourceRange.end.offset.should == 56;
+        }
     }
 }
